d478119acdcf8e4fef3ee62bf123198ffd38e916
Bryan Kizer <bryan.kizer@hypergiant.com>
2024-09-26 10:20:17 -0400

    refactor: remove the academic level files
    To be re-added at a later date. Likely simplified.

--- packages/core/src/category/utils/join-helper/index.ts
+++
@@ -1,9 +0,0 @@
-import type { Chain_ } from '../../functors/3b_chain';
-
-/**
- * Calls a joinable's (Chain) `join` function.
- *
- * join :: Chain f => f (f a) -> f a
- */
-export const joinHelper = <A, T extends Chain_<A>>(joinable: T) =>
-  joinable.join();
