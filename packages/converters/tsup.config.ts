3b4b8d1bd019affc18afe5253bd1228eef367e4d
Ray Knight <array.knight+github@gmail.com>
2024-10-19 05:28:02 -0700

    fix: 🐛 get file path imports to work

--- packages/converters/tsup.config.ts
+++ packages/converters/tsup.config.ts
@@ -1,13 +1,17 @@
+import { esbuildPluginFilePathExtensions } from 'esbuild-plugin-file-path-extensions';
 import { defineConfig } from 'tsup';
 
 export default defineConfig({
-  // entry: ['src/**/*.{ts,tsx}', '!src/**/*.{d,stories,test}.{ts,tsx}'],
-  // bundle: false,
-  entry: ['src/index.ts'],
+  esbuildPlugins: [
+    esbuildPluginFilePathExtensions({
+      esmExtension: 'js',
+    }),
+  ],
+  entry: ['src/**/*.{ts,tsx}', '!src/**/*.{d,stories,test}.{ts,tsx}'],
+  clean: true,
   dts: true,
   format: 'esm',
   sourcemap: true,
   splitting: true,
   treeshake: true,
-  clean: true,
 });
