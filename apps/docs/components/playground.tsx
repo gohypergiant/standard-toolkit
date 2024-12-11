af289108cdeceb55c7796bbe6f2b09093f218a24
Joshua T Kalis <josh.kalis@hypergiant.com>
2024-12-11 11:43:03 -0600

    save point; need to figure out linked/imported files/modules

--- apps/docs/components/playground.tsx
+++ apps/docs/components/playground.tsx
@@ -3,23 +3,27 @@ import type React from 'react';
 import type { PropsWithChildren } from 'react';
 
 type Props = PropsWithChildren<{
-  context: object;
+  files: Record<string, object>;
 }>;
 
-export default function Playground({ children }: Props) {
-  // props.context
+export default function Playground({ children, files }: Props) {
+  console.log(files);
+
   const code = (children as React.ReactElement).props.children.props.children;
 
   // const [value, setValue] = useState(code);
 
   return (
     <figure>
-      {children}
+      {/* {children} */}
 
       <MonacoEditor
         defaultLanguage='typescript'
         defaultValue={code}
         height='24em'
+        options={{
+          minimap: { enabled: false },
+        }}
         theme='vs-dark'
       />
     </figure>
