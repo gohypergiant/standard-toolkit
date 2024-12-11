2dde8898e7f0e191286424d2484a4c8660aa56d9
Joshua T Kalis <josh.kalis@hypergiant.com>
2024-12-11 15:38:25 -0600

    save point: compiled ts code differs in type from source

--- packages/geo/src/coordinates/latlon/degrees-minutes-seconds/parser.ts
+++ packages/geo/src/coordinates/latlon/degrees-minutes-seconds/parser.ts
@@ -10,7 +10,7 @@
  * governing permissions and limitations under the License.
  */
 
-import * as Patterning from '../../../patterning';
+import * as Patterning from '@/patterning';
 
 import {
   BEARINGS,
