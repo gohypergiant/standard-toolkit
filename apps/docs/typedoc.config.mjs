ef7e2edb64ba92a6342956a4d32033269f2f1262
orteth01 <teddy.ortega@hypergiant.com>
2025-01-03 10:06:33 -0700

    chore(docs): use typedoc directly (#146)
    * chore: remove typedoc plugin
    
    * stopping point
    
    * use src/public for now
    
    * chore: sort scripts
    
    * chore: specify design system as dep of docs
    
    * fix: pnpm-lock
    
    * fix: ls-lint ignore generated docs files

---
+++ apps/docs/typedoc.config.mjs
@@ -0,0 +1,20 @@
+/** @type {Partial<import("typedoc").TypeDocOptions>} */
+const config = {
+  plugin: ['typedoc-plugin-markdown'],
+  entryPointStrategy: 'packages',
+  entryPoints: ['../../packages/*'],
+  packageOptions: {
+    entryPoints: ['src/index.ts'],
+  },
+  out: 'src/public/typedoc',
+
+  // properties taken from plugin-typedoc https://github.com/web-infra-dev/rspress/blob/main/packages/plugin-typedoc/src/index.ts#L40-L53
+  theme: 'markdown',
+  disableSources: true,
+  readme: 'none',
+  githubPages: false,
+  requiredToBeDocumented: ['Class', 'Function', 'Interface'],
+  hideBreadcrumbs: true,
+};
+
+export default config;
