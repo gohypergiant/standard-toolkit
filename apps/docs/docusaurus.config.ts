bc220bfce529fa9b71e605a0a5e1213a449565a4
Teddy Ortega <teddy.ortega@hypergiant.com>
2025-01-08 10:51:19 -0600

    chore: more cleanup

--- apps/docs/docusaurus.config.ts
+++ apps/docs/docusaurus.config.ts
@@ -46,7 +46,7 @@ const config: Config = {
       {
         docs: {
           sidebarPath: './sidebars.ts',
-          // Remove this to remove the "edit this page" links.
+          // Add this to enable the "edit this page" links.
           // editUrl: 'https://github.com/facebook/docusaurus/tree/main/docs/',
         },
         theme: {
@@ -92,23 +92,6 @@ const config: Config = {
             },
           ],
         },
-        // {
-        //   title: 'Community',
-        //   items: [
-        //     {
-        //       label: 'Stack Overflow',
-        //       href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-        //     },
-        //     {
-        //       label: 'Discord',
-        //       href: 'https://discordapp.com/invite/docusaurus',
-        //     },
-        //     {
-        //       label: 'X',
-        //       href: 'https://x.com/docusaurus',
-        //     },
-        //   ],
-        // },
         {
           title: 'More',
           items: [
