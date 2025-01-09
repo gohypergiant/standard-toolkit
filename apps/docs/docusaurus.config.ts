de079e14bd51d6c510b8a40d9348aac5d7c0be1a
Teddy Ortega <teddy.ortega@hypergiant.com>
2025-01-09 15:05:37 -0600

    feat: katex support

--- apps/docs/docusaurus.config.ts
+++ apps/docs/docusaurus.config.ts
@@ -13,6 +13,8 @@
 import { themes as prismThemes } from 'prism-react-renderer';
 import type { Config } from '@docusaurus/types';
 import type * as Preset from '@docusaurus/preset-classic';
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
 
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
@@ -46,6 +48,8 @@ const config: Config = {
       {
         docs: {
           sidebarPath: './sidebars.ts',
+          remarkPlugins: [remarkMath],
+          rehypePlugins: [rehypeKatex],
           // Add this to enable the "edit this page" links.
           // editUrl: 'https://github.com/facebook/docusaurus/tree/main/docs/',
         },
@@ -112,6 +116,16 @@ const config: Config = {
         hashed: true,
       }),
     ],
+  ],
+
+  stylesheets: [
+    {
+      href: 'https://cdn.jsdelivr.net/npm/katex@0.13.24/dist/katex.min.css',
+      type: 'text/css',
+      integrity:
+        'sha384-odtC+0UGzzFL/6PNoE8rX/SPcQDXBJ+uRepguP4QkPCm2LBxH3FA3y+fKSiJ+AmM',
+      crossorigin: 'anonymous',
+    },
   ],
 };
 
