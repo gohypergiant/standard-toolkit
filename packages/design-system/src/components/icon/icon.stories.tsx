5dc377793af2816ae14fb77acb7d9aa4c445dd30
Teddy Ortega <teddy.ortega@hypergiant.com>
2025-01-14 13:48:27 -0600

    chore: icon

--- packages/design-system/src/components/icon/icon.stories.tsx
+++ packages/design-system/src/components/icon/icon.stories.tsx
@@ -10,26 +10,36 @@
  * governing permissions and limitations under the License.
  */
 
-import type { Story, StoryDefault } from '@ladle/react';
-import { Icon } from '../icon';
+import type { StoryObj, Meta } from '@storybook/react';
+import { Icon } from '.';
 import type { IconProps } from './types';
 
-export default {
-  title: 'Components / Icon',
+const meta: Meta = {
+  title: 'Components/Icon',
+  component: Icon,
+  tags: ['autodocs'],
+};
+
+export default meta;
+
+export const Default: StoryObj<IconProps> = {
+  args: {
+    size: 'relative',
+  },
   argTypes: {
     color: {
       control: {
-        type: 'text',
+        type: 'color',
       },
     },
     fill: {
       control: {
-        type: 'text',
+        type: 'color',
       },
     },
     stroke: {
       control: {
-        type: 'text',
+        type: 'color',
       },
     },
     size: {
@@ -40,17 +50,16 @@ export default {
       defaultValue: 'relative',
     },
   },
-} satisfies StoryDefault<IconProps>;
-
-export const Example: Story<IconProps> = (props) => (
-  <Icon {...props}>
-    <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24'>
-      <title>Ladle</title>
-      <path
-        d='M3.559 4.544c.355-.35.834-.544 1.33-.544H19.11c.496 0 .975.194 1.33.544.356.35.559.829.559 1.331v9.25c0 .502-.203.981-.559 1.331-.355.35-.834.544-1.33.544H15.5l-2.7 3.6a1 1 0 0 1-1.6 0L8.5 17H4.889c-.496 0-.975-.194-1.33-.544A1.868 1.868 0 0 1 3 15.125v-9.25c0-.502.203-.981.559-1.331ZM7.556 7.5a1 1 0 1 0 0 2h8a1 1 0 0 0 0-2h-8Zm0 3.5a1 1 0 1 0 0 2H12a1 1 0 1 0 0-2H7.556Z'
-        clipRule='evenodd'
-        fillRule='evenodd'
-      />
-    </svg>
-  </Icon>
-);
+  render: (props) => (
+    <Icon {...props}>
+      <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24'>
+        <title>Ladle</title>
+        <path
+          d='M3.559 4.544c.355-.35.834-.544 1.33-.544H19.11c.496 0 .975.194 1.33.544.356.35.559.829.559 1.331v9.25c0 .502-.203.981-.559 1.331-.355.35-.834.544-1.33.544H15.5l-2.7 3.6a1 1 0 0 1-1.6 0L8.5 17H4.889c-.496 0-.975-.194-1.33-.544A1.868 1.868 0 0 1 3 15.125v-9.25c0-.502.203-.981.559-1.331ZM7.556 7.5a1 1 0 1 0 0 2h8a1 1 0 0 0 0-2h-8Zm0 3.5a1 1 0 1 0 0 2H12a1 1 0 1 0 0-2H7.556Z'
+          clipRule='evenodd'
+          fillRule='evenodd'
+        />
+      </svg>
+    </Icon>
+  ),
+};
