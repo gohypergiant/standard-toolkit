5c3d8eda85b760021d84a9286dea36ba7f58ff55
Teddy Ortega <teddy.ortega@hypergiant.com>
2025-01-14 14:58:27 -0600

    chore: radio

--- packages/design-system/src/components/radio/radio.stories.tsx
+++ packages/design-system/src/components/radio/radio.stories.tsx
@@ -10,64 +10,68 @@
  * governing permissions and limitations under the License.
  */
 
-import type { Story, StoryDefault } from '@ladle/react';
+import type { StoryObj, Meta } from '@storybook/react';
 import { AriaLabel } from '../aria';
 import { Radio, RadioGroup } from './radio';
 import type { RadioGroupProps } from './types';
 
-export default {
-  title: 'Components / RadioGroup',
+const meta: Meta = {
+  title: 'Components/RadioGroup',
+  component: RadioGroup,
+  tags: ['autodocs'],
+  args: {
+    label: 'Selection List',
+    alignInput: 'end',
+    orientation: 'vertical',
+    isDisabled: false,
+    isReadOnly: false,
+    isInvalid: false,
+  },
   argTypes: {
     label: {
       control: {
         type: 'text',
       },
-      defaultValue: 'Selection List',
     },
     alignInput: {
       control: {
         type: 'select',
       },
       options: ['start', 'end'],
-      defaultValue: 'end',
     },
     orientation: {
       control: {
         type: 'select',
       },
       options: ['horizontal', 'vertical'],
-      defaultValue: 'vertical',
     },
     isDisabled: {
       control: {
         type: 'boolean',
       },
-      defaultValue: false,
     },
     isReadOnly: {
       control: {
         type: 'boolean',
       },
-      defaultValue: false,
     },
     isInvalid: {
       control: {
         type: 'boolean',
       },
-      defaultValue: false,
     },
   },
-} satisfies StoryDefault;
+};
 
-export const Example: Story<RadioGroupProps> = ({
-  label,
-  alignInput,
-  ...rest
-}) => (
-  <RadioGroup {...rest} alignInput={alignInput}>
-    <AriaLabel>{label}</AriaLabel>
-    <Radio value='this'>This</Radio>
-    <Radio value='that'>That</Radio>
-    <Radio value='other'>The Other Thing</Radio>
-  </RadioGroup>
-);
+export default meta;
+
+export const Example: StoryObj<RadioGroupProps> = {
+  render: ({ label, alignInput, ...rest }) => (
+    <RadioGroup {...rest} alignInput={alignInput}>
+      <AriaLabel>{label}</AriaLabel>
+      <Radio value='this'>This</Radio>
+      <Radio value='that'>That</Radio>
+      <Radio value='other'>The Other Thing</Radio>
+    </RadioGroup>
+  ),
+};
