abdaf21fd5298decf3560e8a74880ec076b27221
Raechel Odom <raechelodom@Raechels-MacBook-Pro.local>
2025-01-22 13:09:57 -0700

    resolves build error

--- packages/design-system/src/components/slider/index.ts
+++ packages/design-system/src/components/slider/index.ts
@@ -1,3 +1,5 @@
+// __private-exports
+
 /*
  * Copyright 2025 Hypergiant Galactic Systems Inc. All rights reserved.
  * This file is licensed to you under the Apache License, Version 2.0 (the "License");
@@ -10,7 +12,6 @@
  * governing permissions and limitations under the License.
  */
 
-// __private-exports
 export {
   Slider,
   SliderBar,
