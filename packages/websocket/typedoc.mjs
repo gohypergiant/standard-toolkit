113ce9beb0444c7b9b5aec56cf30deb5fbb63c43
Teddy Ortega <teddy.ortega@hypergiant.com>
2025-02-18 10:58:07 -0600

    feat: non-react playgrounds for docs (#171)
    * feat: non-react playgrounds for docs
    
    * refactor: function name
    
    * tweak
    
    * types
    
    * Update packages/math/typedoc.mjs
    
    * chore: blockTags option should be in packageOptions object
    
    * chore: better file name

--- packages/websocket/typedoc.mjs
+++ packages/websocket/typedoc.mjs
@@ -1,5 +1,5 @@
 /*
- * Copyright 2024 Hypergiant Galactic Systems Inc. All rights reserved.
+ * Copyright 2025 Hypergiant Galactic Systems Inc. All rights reserved.
  * This file is licensed to you under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License. You may obtain a copy
  * of the License at https://www.apache.org/licenses/LICENSE-2.0
@@ -10,21 +10,7 @@
  * governing permissions and limitations under the License.
  */
 
-/**
- * Convert a boolean to an integer
- *
- * @remarks
- * pure function
- *
- * @playground
- * import { booleanToNumber } from '@accelint/converters';
- *
- * console.log(booleanToNumber(true));
- * // 1
- *
- * console.log(booleanToNumber(false));
- * // 0
- */
-export function booleanToNumber(val: boolean) {
-  return (val as unknown as number) | 0;
-}
+/** @type {Partial<import("typedoc").TypeDocOptions>} */
+export default {
+  name: '@accelint/websocket',
+};
