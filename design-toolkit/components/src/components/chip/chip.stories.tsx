8d5b3facdc85bd5d04a3e28421a3f9b44f4e9285
Jacob Foster <dalindeck@gmail.com>
2025-05-05 17:04:14 -0700

    feat: add in the chips

---
+++ design-toolkit/components/src/components/chip/chip.stories.tsx
@@ -0,0 +1,134 @@
+/*
+ * Copyright 2025 Hypergiant Galactic Systems Inc. All rights reserved.
+ * This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License. You may obtain a copy
+ * of the License at https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ * OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+
+import type { Meta, StoryObj } from '@storybook/react';
+import { useState } from 'react';
+import { Button } from '../button';
+import { Chip } from './index';
+
+const meta: Meta<typeof Chip> = {
+  title: 'Components/Chip',
+  component: Chip,
+  args: {
+    className: undefined,
+    children: 'Badge text',
+    size: 'medium',
+    variant: 'info',
+  },
+  argTypes: {
+    className: { type: 'string' },
+    children: {
+      control: 'text',
+    },
+    size: {
+      control: 'select',
+      options: ['medium', 'small'],
+    },
+    variant: {
+      control: 'select',
+      options: ['info', 'normal', 'serious', 'critical', 'advisory'],
+    },
+  },
+};
+
+export default meta;
+type Story = StoryObj<typeof Chip>;
+
+export const Default: Story = {
+  render: ({ children, ...args }) => <Chip {...args}>{children}</Chip>,
+};
+
+export const List: Story = {
+  render: () => (
+    <Chip.List>
+      <Chip variant='critical'>Out of gas</Chip>
+      <Chip variant='advisory'>Convoy</Chip>
+      <Chip variant='advisory'>Priority</Chip>
+    </Chip.List>
+  ),
+};
+
+const selectableData = [
+  {
+    id: 'chip-1',
+    isDisabled: false,
+    name: 'Selectable chip',
+  },
+  {
+    id: 'chip-2',
+    isDisabled: true,
+    name: 'Selectable chip',
+  },
+  {
+    id: 'chip-3',
+    isDisabled: false,
+    name: 'Selectable chip',
+  },
+];
+
+export const SelectableChipList: Story = {
+  render: () => (
+    <Chip.List items={selectableData} selectionMode='multiple'>
+      {(item) => (
+        <Chip.Selectable id={item.id} isDisabled={item.isDisabled}>
+          {item.name}
+        </Chip.Selectable>
+      )}
+    </Chip.List>
+  ),
+};
+
+const deletableData = [
+  {
+    id: 'chip-1',
+    name: 'Deletable chip 1',
+  },
+  {
+    id: 'chip-2',
+    name: 'Deletable chip 2',
+  },
+  {
+    id: 'chip-3',
+    name: 'Deletable chip 3',
+  },
+];
+
+export const DeletableChipList: Story = {
+  render: () => {
+    const [items, setItems] = useState(deletableData);
+
+    return (
+      <div className='flex flex-col items-center gap-m'>
+        <Chip.List
+          items={items}
+          selectionMode='multiple'
+          onRemove={(ids) =>
+            setItems((items) => items.filter((i) => !ids.has(i.id)))
+          }
+        >
+          {(item) => (
+            <Chip.Deletable id={item.id} size='small'>
+              {item.name}
+            </Chip.Deletable>
+          )}
+        </Chip.List>
+        <Button
+          variant='flat'
+          size='xsmall'
+          onPress={() => setItems(deletableData)}
+        >
+          Reset
+        </Button>
+      </div>
+    );
+  },
+};
---
+++ design-toolkit/components/src/components/chip/index.tsx
@@ -0,0 +1,244 @@
+/*
+ * Copyright 2025 Hypergiant Galactic Systems Inc. All rights reserved.
+ * This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License. You may obtain a copy
+ * of the License at https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ * OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+
+import { CancelFill } from '@/icons';
+import { cn } from '@/lib/utils';
+import { type VariantProps, cva } from 'cva';
+import type React from 'react';
+import { createContext, useContext } from 'react';
+import {
+  Tag as AriaTag,
+  TagGroup as AriaTagGroup,
+  type TagGroupProps as AriaTagGroupProps,
+  TagList as AriaTagList,
+  type TagListProps as AriaTagListProps,
+  type TagProps as AriaTagProps,
+  Button,
+} from 'react-aria-components';
+
+const chipStyles = cva(
+  cn([
+    'fg-default-light inline-flex items-center justify-center rounded-full border',
+  ]),
+  {
+    variants: {
+      variant: {
+        advisory: 'border-advisory-bold bg-advisory-subtle',
+        critical: 'border-critical bg-critical-subtle',
+        serious: 'border-serious bg-serious-subtle',
+        normal: 'border-normal bg-normal-subtle',
+        info: 'border-info-bold bg-info-subtle',
+      },
+      size: {
+        medium: 'px-s py-xs text-body-s',
+        small: 'px-s py-xs text-body-xs',
+      },
+    },
+    defaultVariants: {
+      size: 'medium',
+      variant: 'info',
+    },
+  },
+);
+
+export interface ChipProps
+  extends VariantProps<typeof chipStyles>,
+    Omit<React.HTMLProps<HTMLSpanElement>, 'children' | 'size'> {
+  className?: string;
+  /** Used to add text to the badge, such as the number of unread notifications. */
+  children?: string;
+}
+
+const Coordinator = createContext(false);
+
+export const Chip = ({
+  className,
+  size = 'medium',
+  variant = 'info',
+  ...props
+}: ChipProps) => {
+  const context = useContext(Coordinator);
+  const Component = context ? AriaTag : 'span';
+
+  return (
+    <Component
+      className={cn(
+        chipStyles({
+          size,
+          variant,
+          className,
+        }),
+      )}
+      {...props}
+    />
+  );
+};
+Chip.displayName = 'Chip';
+Chip.as = (
+  props: VariantProps<typeof chipStyles>,
+  className?: string | string[],
+) => cn(chipStyles({ ...props, className }));
+
+export interface ChipListProps<T>
+  extends Omit<AriaTagGroupProps, 'children'>,
+    Pick<AriaTagListProps<T>, 'items' | 'children' | 'renderEmptyState'> {}
+
+function ChipList<T extends object>({
+  children,
+  className,
+  items,
+  renderEmptyState,
+  ...props
+}: ChipListProps<T>) {
+  return (
+    <Coordinator.Provider value={true}>
+      <AriaTagGroup {...props}>
+        <AriaTagList<T>
+          items={items}
+          renderEmptyState={renderEmptyState}
+          className={cn('flex flex-wrap gap-xs', className)}
+        >
+          {children}
+        </AriaTagList>
+      </AriaTagGroup>
+    </Coordinator.Provider>
+  );
+}
+ChipList.displayName = 'Chip.List';
+Chip.List = ChipList;
+
+const selectableChipStyles = cva(
+  cn([
+    'fg-default-light inline-flex items-center justify-center rounded-full border border-interactive hover:border-interactive-hover',
+    'ai-selected:border-highlight ai-selected:bg-highlight-subtle',
+  ]),
+  {
+    variants: {
+      isDisabled: {
+        true: 'fg-disabled ai-selected:border-interactive-disabled border-interactive-disabled ai-selected:bg-transparent hover:border-interactive-disabled',
+        false: 'cursor-pointer',
+      },
+      size: {
+        medium: 'px-s py-xs text-body-s',
+        small: 'px-s py-xs text-body-xs',
+      },
+    },
+    defaultVariants: {
+      isDisabled: false,
+      size: 'medium',
+    },
+  },
+);
+
+interface SelectableChipProps
+  extends VariantProps<typeof selectableChipStyles>,
+    Omit<AriaTagProps, 'isDisabled'> {}
+
+export const SelectableChip = ({
+  className,
+  isDisabled = false,
+  size = 'medium',
+  ...props
+}: SelectableChipProps) => (
+  <AriaTag
+    className={cn(
+      selectableChipStyles({
+        isDisabled,
+        size,
+        className,
+      }),
+    )}
+    {...props}
+  />
+);
+SelectableChip.displayName = 'Chip.Selectable';
+Chip.Selectable = SelectableChip;
+
+const deletableChipStyles = cva(
+  cn([
+    'fg-default-light group inline-flex items-center justify-center gap-xs rounded-full border border-interactive hover:border-interactive-hover',
+  ]),
+  {
+    variants: {
+      isDisabled: {
+        true: 'fg-disabled border-interactive-disabled hover:border-interactive-disabled',
+        false: '',
+      },
+      size: {
+        medium: 'p-xs pl-m text-body-s',
+        small: 'p-xs pl-s text-body-xs',
+      },
+    },
+    defaultVariants: {
+      isDisabled: false,
+      size: 'medium',
+    },
+  },
+);
+
+interface DeletableChipProps
+  extends VariantProps<typeof deletableChipStyles>,
+    Omit<AriaTagProps, 'isDisabled'> {}
+
+export const DeletableChip = ({
+  children,
+  className,
+  isDisabled = false,
+  size = 'medium',
+  textValue,
+  ...props
+}: DeletableChipProps) => {
+  const internalTextValue =
+    textValue ?? (typeof children === 'string' ? children : undefined);
+
+  return (
+    <AriaTag
+      className={cn(
+        deletableChipStyles({
+          isDisabled,
+          size,
+          className,
+        }),
+      )}
+      textValue={internalTextValue}
+      {...props}
+    >
+      {({ allowsRemoving, ...props }) => {
+        if (!allowsRemoving) {
+          throw new Error(
+            'You have a <Chip.Deletable> in a <Chip.List> does not specify an onRemove handler.',
+          );
+        }
+
+        return (
+          <>
+            {typeof children === 'function'
+              ? children({ allowsRemoving, ...props })
+              : children}
+            <Button
+              slot='remove'
+              className={cn([
+                'icon-size-[15px] icon-default-dark group-hover:icon-default-light cursor-pointer',
+                isDisabled &&
+                  'icon-disabled group-hover:icon-disabled cursor-not-allowed',
+              ])}
+            >
+              <CancelFill />
+            </Button>
+          </>
+        );
+      }}
+    </AriaTag>
+  );
+};
+DeletableChip.displayName = 'Chip.Deletable';
+Chip.Deletable = DeletableChip;
