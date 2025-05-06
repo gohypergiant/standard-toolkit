10c4afbfc73e7a91d12af1297202fba2f9f564a3
Brenna Switzer <brenna.switzer@hypergiant.com>
2025-05-06 13:16:20 -0700

    feat: add icons package and icons

---
+++ design-toolkit/icons/src/icons/ArrageTable.tsx
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2025 Hypergiant Galactic Systems Inc. All rights reserved.
+ * This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License. You may obtain a copy
+ * of the License at https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ * OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+
+/**
+ * THIS IS A GENERATED FILE. DO NOT ALTER DIRECTLY.
+ */
+
+interface SVGRProps {
+  title?: string;
+  titleId?: string;
+}
+const SvgArrageTable = ({ title, titleId }: SVGRProps) => (
+  <svg
+    xmlns='http://www.w3.org/2000/svg'
+    width={24}
+    height={24}
+    fill='none'
+    aria-labelledby={titleId}
+  >
+    {title ? <title id={titleId}>{title}</title> : null}
+    <path
+      fill='currentColor'
+      d='M4 19V5h4c-.008 4.17 0 14 0 14zM10 19V5h4c-.008 4.17 0 14 0 14zM16 19V5h4c-.008 4.17 0 14 0 14z'
+    />
+  </svg>
+);
+export default SvgArrageTable;
