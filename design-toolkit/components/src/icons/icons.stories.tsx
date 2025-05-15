41a079ee4a2dffbd2150b90851c4462dfda0f440
Brenna Switzer <brenna.switzer@hypergiant.com>
2025-05-15 11:25:41 -0700

    chore: update story to be less wonky

--- design-toolkit/components/src/icons/icons.stories.tsx
+++ design-toolkit/components/src/icons/icons.stories.tsx
@@ -1,5 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import { startCase } from 'lodash';
+import { Icon } from '../components/icon';
 /*
  * Copyright 2025 Hypergiant Galactic Systems Inc. All rights reserved.
  * This file is licensed to you under the Apache License, Version 2.0 (the "License");
@@ -23,21 +24,23 @@ type Story = StoryObj;
 export const UIIcons: Story = {
   render: () => {
     return (
-      <div className='flex flex-col gap-xl'>
+      <>
         {Object.entries(catalog).map(([section, meta]) => (
           <div key={section} className='flex flex-col gap-xl'>
-            <h1 className='fg-interactive-default text-header-xl'>
+            <h1 className='fg-interactive-default mt-xl text-header-xl'>
               {startCase(section)}
             </h1>
             <p className='fg-interactive-hover-light text-body-s'>
               {meta.description}
             </p>
-            <div className='fg-interactive-default grid auto-cols-max grid-cols-2 justify-center gap-l font-display text-body-xs md:grid-cols-4'>
+            <div className='fg-interactive-default grid auto-cols-max grid-cols-2 justify-center gap-l font-display text-body-xs md:grid-cols-3 lg:grid-cols-4'>
               {meta.icons.map((icon) => {
                 return (
-                  <div className='flex items-center gap-l' key={icon.name}>
-                    <span>{icon.icon}</span>
-                    <span key={icon.name}>
+                  <div className='flex items-center gap-s' key={icon.name}>
+                    <Icon className='fg-default-light flex-none'>
+                      {icon.icon}
+                    </Icon>
+                    <span className='flex-none' key={icon.name}>
                       {startCase(icon.name).replaceAll(' ', '')}
                     </span>
                   </div>
@@ -46,7 +49,7 @@ export const UIIcons: Story = {
             </div>
           </div>
         ))}
-      </div>
+      </>
     );
   },
 };
