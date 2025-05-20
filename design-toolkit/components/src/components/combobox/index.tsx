0c08910b36b979050d0dad43d12ce6402856f218
Brenna Switzer <brenna.switzer@hypergiant.com>
2025-05-20 10:00:32 -0700

    chore: remove the multiselect science

--- design-toolkit/components/src/components/combobox/index.tsx
+++ design-toolkit/components/src/components/combobox/index.tsx
@@ -185,10 +185,7 @@ export function ComboBox<T extends MenuItem>({
             </AriaText>
           )}
           <AriaPopover className='w-(--trigger-width)'>
-            <AriaListBox
-              selectionMode='multiple'
-              className='grid max-h-[200px] grid-cols-[auto_1fr] overflow-y-auto overflow-x-clip rounded-medium bg-surface-overlay shadow-elevation-overlay outline outline-static-light'
-            >
+            <AriaListBox className='grid max-h-[200px] grid-cols-[auto_1fr] overflow-y-auto overflow-x-clip rounded-medium bg-surface-overlay shadow-elevation-overlay outline outline-static-light'>
               {/* @ts-expect-error package version mismatch TODO */}
               {children}
             </AriaListBox>
