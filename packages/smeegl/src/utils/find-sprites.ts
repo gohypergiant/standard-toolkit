7d7cad08fa61a725875a5a0a82a6f020d4be5f08
Bryan Kizer <bryan.kizer@hypergiant.com>
2025-05-22 17:14:39 -0400

    refactor: split into util folder

---
+++ packages/smeegl/src/utils/find-sprites.ts
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2025 Hypergiant Galactic Systems Inc. All rights reserved.
+ * This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License. You may obtain a copy
+ * of the License at https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ * OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+
+import { globby } from 'globby';
+import { Result } from 'true-myth';
+import type { GlobResultPromise } from './types.js';
+
+const IGNORE_LIST = [
+  // NOTE: For now, ignore. Will need later?
+  '**/node_modules/**',
+  '**/__{fixtures,mocks,tests}__/**',
+];
+
+export async function findSprites(
+  glob: string,
+  rootPath: string,
+): GlobResultPromise {
+  try {
+    const result = await globby(glob, { ignore: IGNORE_LIST, cwd: rootPath });
+
+    return result.length ? Result.ok(result) : Result.err('No sprites found');
+  } catch (err) {
+    return Result.err((err as Error).message);
+  }
+}
