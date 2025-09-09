44bdedd1a18bb8989060e57a26c6b9c78a50ef7b
Brandon Pierce <brandonpierce91@gmail.com>
2025-09-08 21:17:37 -0400

    feat: add more examples

--- apps/next/src/app/log-test.ts
+++ apps/next/src/app/log-test.ts
@@ -11,29 +11,56 @@
  */
 
 import { getLogger } from '@accelint/logger';
+import { openTelemetryPlugin } from '@loglayer/plugin-opentelemetry';
+import { HttpTransport } from '@loglayer/transport-http';
+
+const isServer = typeof window === 'undefined';
 
 export const logger = getLogger({
   enabled: true,
   level: 'trace',
   prefix: '[@apps/next]',
-  pretty: false,
+  pretty: true,
+  transports: [
+    new HttpTransport({
+      enabled: !isServer,
+      url: '/api/otel',
+      method: 'POST',
+      enableBatchSend: false,
+      payloadTemplate: ({ logLevel, message, data }) =>
+        JSON.stringify({
+          timestamp: new Date().toISOString(),
+          level: logLevel,
+          message,
+          metadata: data,
+        }),
+    }),
+  ],
+  plugins: [
+    openTelemetryPlugin({
+      traceFieldName: 'trace',
+      traceIdFieldName: 'traceId',
+      spanIdFieldName: 'spanId',
+      traceFlagsFieldName: 'flags',
+    }),
+  ],
 });
 
 const comment = {
   postId: 1,
   id: 3,
   name: 'odio adipisci rerum aut animi',
-  email: 'Nikita@garfield.biz',
+  email: 'foo@bar.biz',
   body: 'quia molestiae reprehenderit quasi aspernatur\naut expedita occaecati aliquam eveniet laudantium\nomnis quibusdam delectus saepe quia accusamus maiores nam est\ncum et ducimus et vero voluptates excepturi deleniti ratione',
 };
 
-const logWithContext = logger.withContext({ comment });
-
 export function testLogs() {
+  const logWithContext = logger.withMetadata({ comment });
   logWithContext.info('Info test...');
-  logWithContext.warn('Warning test...');
-  logWithContext.error('Error test...');
   logWithContext.debug('Debug test...');
-  logWithContext.trace('Trace test...');
-  logWithContext.fatal('Fatal test...');
+  // These are a bit noisy
+  // logWithContext.warn('Warning test...');
+  // logWithContext.trace('Trace test...');
+  // logWithContext.withError(new Error('Error Test!')).error('Error test...');
+  // logWithContext.fatal('Fatal test...');
 }
