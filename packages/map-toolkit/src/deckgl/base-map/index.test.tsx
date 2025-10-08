c639fc932bd23c00da9b90011a7e12e098ab4843
Cole Hart <cole.hart@hypergiant.com>
2025-10-08 12:10:34 -0600

    test: add basemap test

---
+++ packages/map-toolkit/src/deckgl/base-map/index.test.tsx
@@ -0,0 +1,33 @@
+/*
+ * Copyright 2025 Hypergiant Galactic Systems Inc. All rights reserved.
+ * This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License. You may obtain a copy
+ * of the License at https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ * OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+
+import { useOn } from '@accelint/bus/react';
+import { render, renderHook } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { describe, expect, it, vi } from 'vitest';
+import { MapEvents } from './events';
+import { BaseMap } from './index';
+import type { MapClickEvent, MapHoverEvent } from './types';
+
+// Mock MapLibre hook since it requires browser APIs
+vi.mock('../../maplibre/hooks/use-maplibre', () => ({
+  useMapLibre: vi.fn(),
+}));
+
+describe('BaseMap', () => {
+  it('should apply className to container', () => {
+    const { container } = render(<BaseMap className='custom-map-class' />);
+
+    const mapContainer = container.querySelector('.custom-map-class');
+    expect(mapContainer).toBeInTheDocument();
+  });
+});
