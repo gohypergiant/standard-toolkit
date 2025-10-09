e8b0227a035fb47a704daf115a047289453468b4
Joshua Mora <josh.mora@hypergiant.com>
2025-10-08 18:31:45 -0700

    feat: safe-enum as part of core package
    Open-source the safe-enum package, making it available to
    consumers as part of the @accelint/core package.

---
+++ packages/core/src/safe-enum/get-safe-enum-values/index.test.ts
@@ -0,0 +1,56 @@
+import { describe, expect, expectTypeOf, it } from 'vitest';
+import { getSafeEnumValues } from '.';
+import type { SafeEnum } from '../safe-enum';
+
+describe('getSafeEnumValues', () => {
+  it('should return the values of a safe enum', () => {
+    const StringEnum = {
+      valueOne: 'valueOne',
+      valueTwo: 'valueTwo',
+    } as const;
+
+    const stringEnumValues = getSafeEnumValues(StringEnum);
+
+    expect(stringEnumValues).toEqual([
+      StringEnum.valueOne,
+      StringEnum.valueTwo,
+    ]);
+  });
+});
+
+describe('getSafeEnumValues types', () => {
+  it('should return the values of a safe enum', () => {
+    const StringEnum = {
+      valueOne: 'valueOne',
+      valueTwo: 'valueTwo',
+    } as const;
+
+    type StringEnum = SafeEnum<typeof StringEnum>;
+
+    const stringEnumValues = getSafeEnumValues(StringEnum);
+
+    type StringEnumValues = readonly [StringEnum, ...StringEnum[]];
+
+    expectTypeOf(stringEnumValues).toEqualTypeOf<StringEnumValues>();
+  });
+
+  it('should fail if enum is not a safe enum', () => {
+    const NotConstantEnum = {
+      valueOne: 'valueOne',
+      valueTwo: 'valueTwo',
+    };
+
+    type NotConstantEnumManual = 'valueOne' | 'valueTwo';
+
+    const notConstantEnumValues = getSafeEnumValues(NotConstantEnum);
+
+    type NotConstantEnumValues = readonly [
+      NotConstantEnumManual,
+      ...NotConstantEnumManual[],
+    ];
+
+    expectTypeOf(
+      notConstantEnumValues,
+    ).not.toEqualTypeOf<NotConstantEnumValues>();
+  });
+});
