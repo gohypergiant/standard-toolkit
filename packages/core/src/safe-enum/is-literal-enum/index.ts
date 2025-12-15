3bd98d024f05f8459d08a5b42b54d203c3c21e47
Brandon Dewitt <brandon.dewitt@hypergiant.com>
2025-12-15 15:03:02 -0500

    chore: formatting

--- packages/core/src/safe-enum/is-literal-enum/index.ts
+++ packages/core/src/safe-enum/is-literal-enum/index.ts
@@ -15,23 +15,27 @@
  *
  * @internal
  */
-type IsLiteralEnumForPrimitive<TObject, TPrimitive> =
-  TObject extends Record<string, TPrimitive>
-    ? keyof TObject extends string
-      ? TObject[keyof TObject] extends TPrimitive
-        ? TPrimitive extends TObject[keyof TObject]
-          ? false
-          : true
-        : false
+type IsLiteralEnumForPrimitive<TObject, TPrimitive> = TObject extends Record<
+  string,
+  TPrimitive
+>
+  ? keyof TObject extends string
+    ? TObject[keyof TObject] extends TPrimitive
+      ? TPrimitive extends TObject[keyof TObject]
+        ? false
+        : true
       : false
-    : false;
+    : false
+  : false;
 
 /**
  * Check that an enum contains values that are only strings or number and values are readonly.
  *
  * @internal
  */
-export type IsLiteralEnum<TObject> =
-  IsLiteralEnumForPrimitive<TObject, string> extends false
-    ? IsLiteralEnumForPrimitive<TObject, number>
-    : true;
+export type IsLiteralEnum<TObject> = IsLiteralEnumForPrimitive<
+  TObject,
+  string
+> extends false
+  ? IsLiteralEnumForPrimitive<TObject, number>
+  : true;
