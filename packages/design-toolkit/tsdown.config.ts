5bc13ea4e0fb52b024adb181cc085e2d22e0bff0
Brandon Pierce <brandonpierce91@gmail.com>
2025-12-16 10:28:30 -0500

    fix: add license banner

--- packages/design-toolkit/tsdown.config.ts
+++ packages/design-toolkit/tsdown.config.ts
@@ -14,6 +14,13 @@ import copy from 'rollup-plugin-copy';
 import { defineConfig } from 'tsdown';
 
 export default defineConfig({
+  plugins: [
+    copy({
+      targets: [{ src: 'src/**/*.css', dest: 'dist' }],
+      flatten: false,
+      hook: 'writeBundle',
+    }),
+  ],
   entry: [
     'src/**/*.{ts,tsx}',
     '!src/**/*.{d,stories,test,test-d,bench}.{ts,tsx}',
@@ -35,11 +42,17 @@ export default defineConfig({
     // we just copy css files manually
     /\.css$/,
   ],
-  plugins: [
-    copy({
-      targets: [{ src: 'src/**/*.css', dest: 'dist' }],
-      flatten: false,
-      hook: 'writeBundle',
-    }),
-  ],
+  outputOptions: {
+    banner: `/*
+ * Copyright 2025 Hypergiant Galactic Systems Inc. All rights reserved.
+ * This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License. You may obtain a copy
+ * of the License at https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ * OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */\n\n`,
+  },
 });
