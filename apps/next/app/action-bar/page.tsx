6462d9d40077dbd79b6fe1bae6bb062e1b1193f0
Brandon Dewitt <brandon.dewitt@hypergiant.com>
2025-12-17 11:06:15 -0500

    feat: add visual regression tests

--- apps/next/app/action-bar/page.tsx
+++ apps/next/app/action-bar/page.tsx
@@ -10,23 +10,16 @@
  * governing permissions and limitations under the License.
  */
 
-import 'server-only';
-import { Notice } from '@accelint/design-toolkit/components/notice';
-import { MEDIUM_VARIANTS, SMALL_VARIANTS } from './variants';
+import { BentoGroup } from '~/components/bento';
+import { ActionBarExample } from '~/features/action-bar';
 
-export function NoticeExampleServer() {
+// biome-ignore lint/style/useNamingConvention: nextjs convention
+export const experimental_ppr = true;
+
+export default function Page() {
   return (
-    <>
-      <div className='flex flex-row flex-wrap gap-m'>
-        {MEDIUM_VARIANTS.map((props, k) => (
-          <Notice key={k} message='This is a notice message' {...props} />
-        ))}
-      </div>
-      <div className='flex flex-row flex-wrap gap-m'>
-        {SMALL_VARIANTS.map((props, k) => (
-          <Notice key={k} message='This is a notice message' {...props} />
-        ))}
-      </div>
-    </>
+    <BentoGroup>
+      <ActionBarExample />
+    </BentoGroup>
   );
 }
