7365c75793e75399317203a48c1c6a8233714849
Francis Tse <francis.tse@hypergiant.com>
2026-01-09 13:15:49 -0800

    fix: description text inconsistency in DateField stories

--- packages/design-toolkit/src/components/date-field/date-field.stories.tsx
+++ packages/design-toolkit/src/components/date-field/date-field.stories.tsx
@@ -9,7 +9,6 @@
  * OF ANY KIND, either express or implied. See the License for the specific language
  * governing permissions and limitations under the License.
  */
-
 import { parseAbsoluteToLocal, parseDateTime } from '@internationalized/date';
 import { DateField } from './index';
 import type { Meta, StoryObj } from '@storybook/react-vite';
@@ -20,13 +19,14 @@ const meta = {
   args: {
     label: 'Label',
     defaultValue: parseDateTime('2020-01-23T00:00:00'),
-    description: 'Format: d MMM yyyy',
+    description: 'Format: MMM dd yyyy',
     errorMessage: '',
     granularity: 'day',
     size: 'medium',
     isDisabled: false,
     isInvalid: false,
     isRequired: true,
+    isReadOnly: false,
   },
   argTypes: {
     size: {
@@ -52,7 +52,7 @@ export const WithoutShortMonth: Story = {
     ...Default.args,
     granularity: 'day',
     shortMonth: false,
-    description: 'Format: d MM yyyy',
+    description: 'Format: mm dd yyyy',
     defaultValue: parseDateTime('2020-01-23T00:00:00'),
   },
   render: DateField,
@@ -62,7 +62,7 @@ export const DateTime: Story = {
   args: {
     ...Default.args,
     granularity: 'second',
-    description: 'Format: d MMM yyyy hh:mm:ss a ZZZZ',
+    description: 'Format: MMM dd yyyy hh:mm:ss a ZZZZ',
     defaultValue: parseAbsoluteToLocal('2021-04-07T18:45:22Z'),
   },
   render: DateField,
