6f05576153cb005f8661be9bc3bfbd474e935027
colehart <cole.hart@hypergiant.com>
2026-01-09 10:59:49 -0700

    chore: more year updates (#731)
    * chore: more year updates
    
    * fix: weird formatting issue

--- packages/map-toolkit/src/decorators/deckgl.tsx
+++ packages/map-toolkit/src/decorators/deckgl.tsx
@@ -1,7 +1,7 @@
 // __private-exports
 
 /*
- * Copyright 2025 Hypergiant Galactic Systems Inc. All rights reserved.
+ * Copyright 2026 Hypergiant Galactic Systems Inc. All rights reserved.
  * This file is licensed to you under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License. You may obtain a copy
  * of the License at https://www.apache.org/licenses/LICENSE-2.0
