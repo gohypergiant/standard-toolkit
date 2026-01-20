db7173a673f1d2f62e1a804f951c134285d35faf
Dax Richardson <dax.richardson@hypergiant.com>
2026-01-20 11:40:24 -0600

    fix: Update drawer.visual for memlab tests. :rabbit:

--- apps/next/src/features/drawer/drawer.visual.tsx
+++ apps/next/src/features/drawer/drawer.visual.tsx
@@ -64,7 +64,7 @@ function DrawerVariantComponent({ variant }: { variant: DrawerVariant }) {
               <DrawerHeaderTitle>
                 {variant.placement} / {variant.menuPosition}
               </DrawerHeaderTitle>
-              <DrawerClose id={ids.viewA} />
+              <DrawerClose for={ids.viewA} />
             </DrawerHeader>
             <DrawerContent>
               <p className='fg-primary'>
@@ -78,7 +78,7 @@ function DrawerVariantComponent({ variant }: { variant: DrawerVariant }) {
           <DrawerView id={ids.viewB}>
             <DrawerHeader>
               <DrawerHeaderTitle>View B</DrawerHeaderTitle>
-              <DrawerClose id={ids.viewB} />
+              <DrawerClose for={ids.viewB} />
             </DrawerHeader>
             <DrawerContent>
               <p className='fg-primary'>Content for View B</p>
@@ -87,7 +87,7 @@ function DrawerVariantComponent({ variant }: { variant: DrawerVariant }) {
           <DrawerView id={ids.viewC}>
             <DrawerHeader>
               <DrawerHeaderTitle>View C</DrawerHeaderTitle>
-              <DrawerClose id={ids.viewC} />
+              <DrawerClose for={ids.viewC} />
             </DrawerHeader>
             <DrawerContent>
               <p className='fg-primary'>Content for View C</p>
