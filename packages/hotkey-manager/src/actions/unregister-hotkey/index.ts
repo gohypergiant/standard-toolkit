4cff4dfec5aece1c45996935c9256048067075e5
Brandon Dewitt <brandon.dewitt@hypergiant.com>
2026-01-27 11:18:54 -0500

    fix: HotkeyManager type

--- packages/hotkey-manager/src/actions/unregister-hotkey/index.ts
+++ packages/hotkey-manager/src/actions/unregister-hotkey/index.ts
@@ -1,5 +1,6 @@
 /*
  * Copyright 2025 Hypergiant Galactic Systems Inc. All rights reserved.
+
  * This file is licensed to you under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License. You may obtain a copy
  * of the License at https://www.apache.org/licenses/LICENSE-2.0
@@ -12,42 +13,73 @@
 
 import { hotkeyStore } from '@/stores/hotkey-store';
 import type { HotkeyConfig } from '@/types/hotkey-config';
-import type { HotkeyHook } from '@/types/hotkey-hook';
 import type { HotkeyId } from '@/types/hotkey-id';
+
+import type { HotkeyManager } from '@/types/hotkey-manager';
 
 /**
  * Unregisters a hotkey.
  *
- * @param id The hotkey id.
+ * @param id - The hotkey id.
+ * @returns void
+ *
+
+ * @example
+
+ * ```typescript
+ * import { unregisterHotkey } from '@accelint/hotkey-manager';
+ *
+ * unregisterHotkey('my-hotkey-id');
+ * ```
  */
 export function unregisterHotkey(id: HotkeyId): void;
 
 /**
  * Unregisters a hotkey.
  *
- * @param config The hotkey config.
+ * @param config - The hotkey config.
+ * @returns void
+ *
+ * @example
+
+ * ```typescript
+ * import { unregisterHotkey } from '@accelint/hotkey-manager';
+ *
+ * const config = { id: 'my-hotkey', key: { code: 'KeyA' } };
+ * unregisterHotkey(config);
+ * ```
  */
 export function unregisterHotkey(config: HotkeyConfig): void;
 
 /**
  * Unregisters a hotkey.
  *
- * @param hook The hotkey hook.
+ * @param manager - The hotkey manager.
+ * @returns void
+ *
+ * @example
+ * ```typescript
+
+ * import { unregisterHotkey } from '@accelint/hotkey-manager';
+ *
+ * const manager = registerHotkey({ key: { code: 'KeyA' } });
+ * unregisterHotkey(manager);
+ * ```
  */
-export function unregisterHotkey(hook: HotkeyHook): void;
+export function unregisterHotkey(manager: HotkeyManager): void;
 export function unregisterHotkey(
-  idOrConfigOrHook: HotkeyId | HotkeyConfig | HotkeyHook,
+  idOrConfigOrManager: HotkeyId | HotkeyConfig | HotkeyManager,
 ): void {
   let id: HotkeyId;
 
   if (
-    (typeof idOrConfigOrHook === 'object' ||
-      idOrConfigOrHook instanceof Function) &&
-    'id' in idOrConfigOrHook
+    (typeof idOrConfigOrManager === 'object' ||
+      typeof idOrConfigOrManager === 'function') &&
+    'id' in idOrConfigOrManager
   ) {
-    id = idOrConfigOrHook.id;
+    id = idOrConfigOrManager.id;
   } else {
-    id = idOrConfigOrHook;
+    id = idOrConfigOrManager;
   }
 
   hotkeyStore.getState().unregisterHotkey(id);
