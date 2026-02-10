c0e1c3e79340e8bcdc01e342ac336356f88546d6
Dax Richardson <dax.richardson@hypergiant.com>
2026-02-10 16:39:24 -0600

    fix: Update link to use generateVariantMatrix. :rabbit:

--- apps/next/src/features/link/link.visual.tsx
+++ apps/next/src/features/link/link.visual.tsx
@@ -1,16 +1,37 @@
-import { BentoGroup } from '~/components/bento';
-import { createVisualTests } from '~/visual-regression/vitest';
-import { LinkServerExample } from './server';
+/*
+ * Copyright 2026 Hypergiant Galactic Systems Inc. All rights reserved.
+ * This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License. You may obtain a copy
+ * of the License at https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ * OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
 
-function LinkVariants() {
-  return (
-    <BentoGroup>
-      <LinkServerExample />
-    </BentoGroup>
-  );
-}
+import type { LinkProps } from '@accelint/design-toolkit';
+import { Link } from '@accelint/design-toolkit';
+import {
+  createInteractiveVisualTests,
+  generateVariantMatrix,
+} from '~/visual-regression/vitest';
 
-createVisualTests({
+const linkVariants = generateVariantMatrix<LinkProps>({
+  dimensions: {
+    allowsVisited: [true, false],
+    isVisited: [true, false],
+    isDisabled: [true, false],
+  },
+  baseProps: {
+    children: 'Example Link',
+  },
+});
+
+createInteractiveVisualTests({
   componentName: 'Link',
-  variantsComponent: LinkVariants,
+  renderComponent: (props) => <Link {...props}>{props.children}</Link>,
+  testId: 'test-link',
+  variants: linkVariants,
+  states: ['default', 'hover', 'focus', 'disabled'],
 });
