8dc881e27f2efa8fb74d6370e62d868de7b8ce5f
Joshua Mora <josh.mora@hypergiant.com>
2026-03-20 00:09:41 -0700

    feat: layout/overflow management hooks
    Adds hooks that will be used for the updated layout/overflow management approach. Updates GanttContextProvider to add two new refs that will be used as part of the layout managemt. Usage of hooks and overall integration of updated layout/overflow management will be done as follow-up work.

---
+++ packages/design-toolkit/src/components/gantt/hooks/use-vertical-scroll-update/index.ts
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2026 Hypergiant Galactic Systems Inc. All rights reserved.
+ * This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License. You may obtain a copy
+ * of the License at https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ * OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+
+import { useGanttContext } from '../../context';
+import { useGanttStore } from '../../context/store';
+import { getVerticalScrolledPixels } from '../../utils/helpers';
+import type { UIEvent } from 'react';
+
+type UseVerticalScrollUpdateValue = {
+  onScroll: (event: UIEvent<HTMLDivElement>) => void;
+};
+
+export function useVerticalScrollUpdate(): UseVerticalScrollUpdateValue {
+  const { ganttContentElement } = useGanttContext();
+  const setCurrentRowScrollPx = useGanttStore(
+    (state) => state.setCurrentRowScrollPx,
+  );
+
+  const onScroll = (event: UIEvent<HTMLDivElement>) => {
+    const scrolledPixels = getVerticalScrolledPixels(event);
+
+    setCurrentRowScrollPx(scrolledPixels);
+    ganttContentElement?.scrollTo({ top: scrolledPixels });
+  };
+
+  return { onScroll };
+}
