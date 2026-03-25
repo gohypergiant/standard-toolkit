b2fabc59e9d8e0dffbdc8e428910b21c3e864d7d
Cole Hart <cole.hart@hypergiant.com>
2026-03-25 12:41:49 -0600

    feat: add new icons

--- packages/icons/src/icons/wheel.tsx
+++ packages/icons/src/icons/wheel.tsx
@@ -19,7 +19,7 @@ interface SVGRProps {
   title?: string;
   titleId?: string;
 }
-const SvgTrackHistory = ({
+const SvgWheel = ({
   title,
   titleId,
   ...props
@@ -34,20 +34,20 @@ const SvgTrackHistory = ({
     {title ? <title id={titleId}>{title}</title> : null}
     <path
       fill='currentColor'
-      d='m16.621 3.742 1.566-.721-.722 1.565-1.172 2.579a7.412 7.412 0 0 0-5.136 11.305l-.587 1.307-.58-1.43-2.062-5.068-5.068-2.063-1.43-.58 1.534-.688z'
+      fillRule='evenodd'
+      d='M12 19.5a7.5 7.5 0 1 0 0-15 7.5 7.5 0 0 0 0 15m0 1.5a9 9 0 1 0 0-18 9 9 0 0 0 0 18'
+      clipRule='evenodd'
+    />
+    <path fill='currentColor' d='M11.25 3h1.5v18h-1.5z' />
+    <path fill='currentColor' d='M20.625 11.25v1.5H3.485v-1.5z' />
+    <path
+      fill='currentColor'
+      d='m17.834 5.106 1.06 1.06L6.166 18.894l-1.06-1.06z'
     />
     <path
       fill='currentColor'
-      fillRule='evenodd'
-      d='M17.41 19.123a4.631 4.631 0 1 0 0-9.263 4.631 4.631 0 0 0 0 9.263m0 1.39a6.02 6.02 0 1 0 0-12.042 6.02 6.02 0 0 0 0 12.041'
-      clipRule='evenodd'
-    />
-    <path
-      fill='currentColor'
-      fillRule='evenodd'
-      d='m16.319 11.019 1.39.001-.005 3.34 2.554 2.344-.94 1.023-3.004-2.758z'
-      clipRule='evenodd'
+      d='m18.894 17.834-1.06 1.06L5.106 6.166l1.06-1.06z'
     />
   </svg>
 );
-export default SvgTrackHistory;
+export default SvgWheel;
