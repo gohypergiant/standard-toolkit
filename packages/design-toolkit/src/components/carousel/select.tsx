118174a5b29297fcbc9a7adf5923b2d9e4e6be8b
Dax <dax.richardson@hypergiant.com>
2026-04-06 14:49:52 -0500

    feat: Carousel Component (#939)
    * feat: Add initial story, more notes! :rabbit:
    
    * feat: More storybook work, rearranging components. :rabbit:
    
    * feat: Update data types, moving components around. More discovery. :rabbit:
    
    * fix: Update types. :rabbit:
    
    * feat: Adding img display for mainviewer and thumbnails. :rabbit:
    
    * feat: Add exports for Carousel components. :rabbit:
    
    * feat: Carousel. :rabbit:
    
    * feat: Continue work on carousel. :rabbit:
    
    * feat: Carousel!! Adding tests. :rabbit:
    
    * fix: Update index. :rabbit:
    
    * chore: Add initial documentation. :rabbit:
    
    * feat: Rearranging the furniture. :rabbit:
    
    * chore: Add tests for carousel components. :rabbit:
    
    * chore: Start expanding on docs. :rabbit:
    
    * fix: Rearranging the furniture a bit, making Carousel more testable. :rabbit:
    
    * chore: Move data to fixtures. :rabbit:
    
    * fix: Update fixtures to use more... reasonable images. :rabbit:
    
    * chore: More docs!! :rabbit:
    
    * chore: Add changeset. :rabbit:
    
    * fix: update styling for viewer img component. :rabbit:
    
    * chore: MR feedback
    
    * fix: support multimedia in carousel
    
    * fix: useLayoutEffect to avoid painting glitch issues
    
    * fix: lift callback out of inline and remove unused const
    
    * fix: clean up tests
    
    * fix: remove unused import
    
    * fix: adjust styles so height is constant
    
    * fix: align select dropdown
    
    * fix: make docs accurate and remove unnecessary classnames prop
    
    * fix: address PR feedback
    
    * fix: the thumbnails are square
    
    ---------
    
    Co-authored-by: Cole Hart <cole.hart@hypergiant.com>
    Co-authored-by: Brenna Switzer <brenna.switzer@hypergiant.com>
    Co-authored-by: switzerb <brenna.switzer@gmail.com>

---
+++ packages/design-toolkit/src/components/carousel/select.tsx
@@ -0,0 +1,75 @@
+/*
+ * Copyright 2026 Hypergiant Galactic Systems Inc. All rights reserved.
+ * This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License. You may obtain a copy
+ * of the License at https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ * OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+
+'use client';
+import 'client-only';
+import { useContext } from 'react';
+import { OptionsItem } from '../options/item';
+import { SelectField } from '../select-field';
+import { CarouselContext } from './context';
+import type { Key } from 'react-aria-components';
+import type { CarouselSelectProps } from './types';
+import styles from './style.module.css';
+import { clsx } from '@accelint/design-foundation/lib/utils';
+
+/**
+ * Renders a dropdown select for navigating the carousel by item title.
+ * Note that it relies on the <Carousel/> component to supply context and must be used
+ * as a child of that component.
+ *
+ * Displays the current item's title and allows the user to jump to any
+ * item in the carousel via a select dropdown.
+ *
+ * @param props - The select props.
+ * @param props.classNames - Custom class names for select elements.
+ * @param props.classNames.field - Class names passed to the SelectField.
+ * @param props.classNames.optionItem - Class names passed to each OptionsItem.
+ * @returns The carousel select component.
+ */
+export function CarouselSelect({ classNames, ...rest }: CarouselSelectProps) {
+  const { items, currentPosition, setCurrentPosition } =
+    useContext(CarouselContext);
+
+  const onChange = (key: Key | null) => {
+    const index = items.findIndex((i) => i.id === key);
+    if (index >= 0) {
+      setCurrentPosition(index);
+    }
+  };
+
+  return (
+    <SelectField
+      {...rest}
+      aria-label='Carousel item select'
+      value={items[currentPosition]?.title}
+      placeholder={items[currentPosition]?.title}
+      onChange={onChange}
+      classNames={{
+        ...classNames?.select,
+        field: clsx(styles['gallery-select'], classNames?.select?.field),
+        trigger: clsx(styles['gallery-select'], classNames?.select?.trigger),
+      }}
+    >
+      {items.map((item) => (
+        <OptionsItem
+          textValue={item.title}
+          key={item.id}
+          aria-label={item.title}
+          id={item.id}
+          classNames={classNames?.optionItem}
+        >
+          {item.title}
+        </OptionsItem>
+      ))}
+    </SelectField>
+  );
+}
