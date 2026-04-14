2aeb96cca8cd0bb5cb25e5c2448a61d223bd366f
switzerb <brenna.switzer@gmail.com>
2026-04-14 10:08:08 -0700

    fix: some memoization

--- packages/design-toolkit/src/components/tree/index.tsx
+++ packages/design-toolkit/src/components/tree/index.tsx
@@ -15,7 +15,7 @@ import { Cache, type CacheTreeNode } from '@/hooks/use-tree/actions/cache';
 import type { Key, Selection } from '@react-types/shared';
 import 'client-only';
 import { clsx } from '@accelint/design-foundation/lib/utils';
-import { useMemo } from 'react';
+import { useCallback, useMemo } from 'react';
 import {
   Tree as AriaTree,
   composeRenderProps,
@@ -26,6 +26,7 @@ import {
 import { TreeContext } from './context';
 import styles from './styles.module.css';
 import type { TreeProps } from './types';
+import { noop } from '@accelint/core';
 
 const defaultRenderDropIndicator = (target: DropTarget) => {
   const isBetweenItems =
@@ -204,13 +205,14 @@ export function Tree<T>({
     visibleKeysProp,
   ]);
 
-  const handleSelectionChange = selectedKeys
-    ? (selection: Selection) => {
-        if (selection !== 'all') {
-          onSelectionChange?.(selection);
-        }
+  const handleSelectionChange = useCallback(
+    (selection: Selection) => {
+      if (selection !== 'all') {
+        onSelectionChange?.(selection);
       }
-    : undefined;
+    },
+    [onSelectionChange],
+  );
 
   return (
     <TreeContext.Provider
@@ -225,7 +227,7 @@ export function Tree<T>({
         visibilityComputedKeys,
         indeterminateKeys,
         isStatic: typeof children !== 'function',
-        onVisibilityChange: onVisibilityChange ?? (() => undefined), // TODO: improve
+        onVisibilityChange: onVisibilityChange ?? noop,
       }}
     >
       <AriaTree
@@ -238,7 +240,7 @@ export function Tree<T>({
         expandedKeys={expandedKeys}
         items={items}
         selectedKeys={selectedKeys}
-        onSelectionChange={handleSelectionChange}
+        onSelectionChange={selectedKeys ? handleSelectionChange : undefined}
         selectionMode={selectionMode}
       >
         {children}
