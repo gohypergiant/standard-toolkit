ad2186fcbfb51d733b7f26d28f852ef6190ae39b
Brandon Pierce <brandonpierce91@gmail.com>
2026-07-14 11:19:31 -0400

    fix: provision new fumadocs setup

--- apps/docs/next.config.mjs
+++ apps/docs/next.config.mjs
@@ -1,29 +1,13 @@
-/*
- * Copyright 2026 Hypergiant Galactic Systems Inc. All rights reserved.
- * This file is licensed to you under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License. You may obtain a copy
- * of the License at https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software distributed under
- * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
- * OF ANY KIND, either express or implied. See the License for the specific language
- * governing permissions and limitations under the License.
- */
-
 import { createMDX } from 'fumadocs-mdx/next';
+
+const withMDX = createMDX();
 
 /** @type {import('next').NextConfig} */
 const config = {
-  cacheComponents: true,
   reactStrictMode: true,
+  cacheComponents: false,
   poweredByHeader: false,
   reactCompiler: true,
 };
-
-const withMDX = createMDX({
-  // customize the config file path
-  // configPath: "source.config.ts"
-});
-
 
 export default withMDX(config);
--- apps/docs/source.config.ts
+++ apps/docs/source.config.ts
@@ -1,19 +1,23 @@
-/*
- * Copyright 2026 Hypergiant Galactic Systems Inc. All rights reserved.
- * This file is licensed to you under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License. You may obtain a copy
- * of the License at https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software distributed under
- * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
- * OF ANY KIND, either express or implied. See the License for the specific language
- * governing permissions and limitations under the License.
- */
+import { defineConfig, defineDocs } from 'fumadocs-mdx/config';
+import { metaSchema, pageSchema } from 'fumadocs-core/source/schema';
 
-import { defineDocs, defineConfig } from 'fumadocs-mdx/config';
-
+// You can customize Zod schemas for frontmatter and `meta.json` here
+// see https://fumadocs.dev/docs/mdx/collections
 export const docs = defineDocs({
-  dir: 'content',
-}) as ReturnType<typeof defineDocs>;
+  dir: 'content/docs',
+  docs: {
+    schema: pageSchema,
+    postprocess: {
+      includeProcessedMarkdown: true,
+    },
+  },
+  meta: {
+    schema: metaSchema,
+  },
+});
 
-export default defineConfig();
+export default defineConfig({
+  mdxOptions: {
+    // MDX options
+  },
+});
