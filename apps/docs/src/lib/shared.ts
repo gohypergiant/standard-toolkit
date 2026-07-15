39f284f53c5507593ab276e56fe0855a23392395
Brandon Pierce <brandonpierce91@gmail.com>
2026-07-15 10:38:13 -0400

    feat: sitemap and robots.txt

--- apps/docs/src/lib/shared.ts
+++ apps/docs/src/lib/shared.ts
@@ -2,6 +2,9 @@ export const appName = 'Lyntris Dev Toolkit';
 export const docsRoute = '/docs';
 export const docsImageRoute = '/og/docs';
 export const docsContentRoute = '/llms.mdx/docs';
+export const baseUrl = process.env.NODE_ENV === 'development'
+    ? new URL('http://localhost:3000')
+    : new URL('https://standard-toolkit.accelint.io');
 
 export const gitConfig = {
   user: 'gohypergiant',
