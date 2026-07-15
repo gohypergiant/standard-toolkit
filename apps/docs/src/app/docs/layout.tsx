a2329bc1ef42b4ff11da9e412980e695b6001231
Brandon Pierce <brandonpierce91@gmail.com>
2026-07-15 10:57:23 -0400

    feat: tabs dropdown

--- apps/docs/src/app/docs/layout.tsx
+++ apps/docs/src/app/docs/layout.tsx
@@ -4,7 +4,23 @@ import { baseOptions } from '@/lib/layout.shared';
 
 export default function Layout({ children }: LayoutProps<'/docs'>) {
   return (
-    <DocsLayout {...baseOptions()} tree={source.getPageTree()} >
+    <DocsLayout {...baseOptions()} tree={source.getPageTree()} tabMode='auto' tabs={[
+      {
+        title: 'Standard Toolkit',
+        description: 'TODO',
+        url: '/docs',
+      },
+      {
+        title: 'Neo Toolkit',
+        description: 'TODO',
+        url: 'https://neo-toolkit.accelint.io',
+      },
+      {
+        title: 'Agent Skills',
+        description: 'TODO',
+        url: 'https://agent-skills.accelint.io',
+      },
+    ]}>
       {children}
     </DocsLayout>
   );
--- apps/docs/src/lib/shared.ts
+++ apps/docs/src/lib/shared.ts
@@ -1,4 +1,4 @@
-export const appName = 'Lyntris Dev Toolkit';
+export const appName = 'Standard Toolkit';
 export const docsRoute = '/docs';
 export const docsImageRoute = '/og/docs';
 export const docsContentRoute = '/llms.mdx/docs';
