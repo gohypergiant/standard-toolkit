90daf1ae87cda383f7316d248f1f03d29fc2af7e
Brandon Pierce <brandonpierce91@gmail.com>
2026-07-15 10:19:12 -0400

    fix: adjust poor code gen

--- apps/docs/src/lib/shared.ts
+++ apps/docs/src/lib/shared.ts
@@ -1,11 +1,10 @@
-export const appName = 'My App';
+export const appName = 'Lyntris Dev Toolkit';
 export const docsRoute = '/docs';
 export const docsImageRoute = '/og/docs';
 export const docsContentRoute = '/llms.mdx/docs';
 
-// fill this with your actual GitHub info, for example:
 export const gitConfig = {
-  user: 'fuma-nama',
-  repo: 'fumadocs',
+  user: 'gohypergiant',
+  repo: 'standard-toolkit',
   branch: 'main',
 };
